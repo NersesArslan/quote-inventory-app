76eb84c485588f556cbcb7ac89168e11626d89a0
NersesArslan <108882259+NersesArslan@users.noreply.github.com>
2024-08-06 06:23:04 -0700

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,64 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Greeting from './components/greeting'
+function App() {
+
+  const [formData, setFormData] = useState({
+    author: '',
+    book: '',
+    quote: ''
+  });
+  
+  const [quote, setQuote] = useState([])
+  // const quoteDisplay = quote.map((quotes) => 
+
+  //  <div>
+  //    <p key={quotes}>{quotes.quote}</p>
+  //    <p>{quotes.author}</p>
+  //  </div>
+  // )
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData(prevState => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    setQuote(prevState =>([
+      ...prevState, 
+      formData
+    ]))
+ 
+    setFormData({author: '', book: '', quote: ''})
+  
+  }
+console.log(quote);
+  return (
+    <>
+     <form action="">
+      <div className='author'>
+        <label htmlFor="Author">Author</label>
+        <input type="text" name="author" value={formData.author} onChange={handleChange}/>
+      </div>
+      <div className="book">
+        <label htmlFor="">Book</label>
+        <input type="text" name="book" value={formData.book} onChange={handleChange}/>
+      </div>
+      <div className="quote">
+        <label htmlFor="">Quote</label>
+        <textarea type="text" name="quote" value={formData.quote} onChange={handleChange}></textarea>
+      </div>
+      <button type="submit" onClick={onSubmit}>Submit</button>
+     </form>
+     
+     
+    </>
+  )
+
+}
+export default App
---
+++ src/components/greeting.jsx
@@ -0,0 +1,5 @@
+function Greeting({text}) {
+    return <h1>{text}</h1>;
+  }
+  
+export default Greeting
